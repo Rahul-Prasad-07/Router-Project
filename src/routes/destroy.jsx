2b5849741b2e111625d805cc7225e8e756fd6599
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-03-13 17:49:33 +0530

    Filter the list

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,7 +2,13 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
-  throw new Error("oh dang!");
-  await deleteContact(params.contactId);
-  return redirect("/");
+
+  try{
+    await deleteContact(params.contactId);
+    return redirect("/");
+
+  }
+  catch(error){
+    throw new Error("oh dang!");
+  }
 }
